e09c7d329e0befc3c529eecb8e7fe352eee6871b
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2023-12-07 16:05:04 +0100

    done log

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import Container from "../Container";
 import Logo from "./Logo";
+import Search from "./Search";
 
 const Navbar = () => {
     return (
@@ -13,7 +14,8 @@ const Navbar = () => {
                     gap-3
                      md:gap-0">
 
-<Logo />
+                        <Logo />
+                        <Search />
                     </div>
                 </Container>
             </div>
