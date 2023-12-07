09526543ae775fdc2100347eed07ca0d62f0cc08
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2023-12-07 15:42:11 +0100

    done log

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -1,12 +1,19 @@
-interface Container {
+'use client';
+
+interface ContainerProps {
     Children: React.ReactNode;
 
 }
 
-const Container: React.FC<ContainerProps> = ({children}) => {
-    return ( 
-        <div>{children}</div>
-     );
+const Container: React.FC<ContainerProps> = ({ children }) => {
+    return (
+        <div className="
+        max-w-[2520px] mx-auto xl:px-20 md:px10 sm:px-2 px-4
+        ">
+
+            {children}
+        </div>
+    );
 }
- 
+
 export default Container;
