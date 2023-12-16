86342a464de8d7a8e937d76556ec86c02b37e2b0
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2023-12-16 19:15:56 +0100

    done flags and countries

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,16 +1,16 @@
 'use client' 
 
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
-import { useMemo } from "react";
 import { useForm, FieldValues } from "react-hook-form";
-
 
 import useRentModal from "@/app/hooks/useRentModal";
 import Modal from "./Modal";
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
+import CountrySelect from "../inputs/CountrySelect";
+import Map from "../Map";
 
 enum STEPS {
     CATEGORY = 0,
@@ -47,6 +47,9 @@ const RentModal = () => {
             
     });
     const category = watch('category');
+    const location = watch('location');
+
+
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
             shouldValidate: true,
@@ -103,11 +106,26 @@ const RentModal = () => {
         </div>
     )
 
+
+    if (step === STEPS.LOCATION) {
+        bodyContent = (
+            <div className=" flex flex-col gap-8">
+                <Heading 
+                    title="Where's your place located?"
+                    subtitle="Help guests find you!"/>
+             <CountrySelect
+                value={location}
+       onChange={(value) => setCustomValue('location', value)} />
+       <Map />
+            </div>
+        )
+    }
+
     return (
         <Modal
             isOpen={rentModal.isOpen}
             onClose={rentModal.onClose}
-            onSubmit={rentModal.onClose}
+            onSubmit={onNext}
             actionLabel={actionLabel}
             secondaryActionLabel={secondaryActionLabel}
             secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
