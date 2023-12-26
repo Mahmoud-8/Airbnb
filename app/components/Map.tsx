87be48a10e9c4261e726223c480ad66e8b33286c
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2023-12-26 20:29:34 +0100

    done

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -6,6 +6,10 @@ import 'leaflet/dist/leaflet.css';
 import markerIcon2x from'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from'leaflet/dist/images/marker-icon.png';
 import markerShadow from'leaflet/dist/images/marker-shadow.png';
+import 'leaflet/dist/images/marker-icon-2x.png';
+import 'leaflet/dist/images/marker-icon.png';
+import 'leaflet/dist/images/marker-shadow.png';
+
 
 //@ts-ignore
 delete L.Icon.Default.prototype._getIconUrl;
