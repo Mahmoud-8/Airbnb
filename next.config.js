e0e11b2b930a9261d6f3b7e4cc80b0a311c09c2c
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2023-12-26 19:01:59 +0100

    done

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        appDir: true,
-    }, images: {
-        domains: [
-            
-            "res.cloudinary.com",
-            'avatars.githubusercontent.com',
-      'lh3.googleusercontent.com'
-        
-        ]
+      appDir: true,
+    },
+    images: {
+      domains: [
+        'res.cloudinary.com', 
+        'avatars.githubusercontent.com',
+        'lh3.googleusercontent.com'
+      ]
     }
-}
-
-module.exports = nextConfig
+  }
+  
+  module.exports = nextConfig
