860f0491c939373bdccbd75cdd26c30bd114f034
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2023-12-27 16:53:07 +0100

    done

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,48 +1,52 @@
 'use client';
 
 import L from 'leaflet';
-import { MapContainer, Marker, TileLayer } from 'react-leaflet'
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
-
+// Import images using require
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
+// Ensure TypeScript recognizes the module types for the images
+import markerIcon2xModule from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIconModule from 'leaflet/dist/images/marker-icon.png';
+import markerShadowModule from 'leaflet/dist/images/marker-shadow.png';
+
 // @ts-ignore
-delete L.Icon.Default.prototype._getIconUrl; 
+delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-    iconUrl: markerIcon,
-    iconRetinaUrl: markerIcon2x,
-    shadowUrl: markerShadow,
+    iconUrl: markerIconModule,
+    iconRetinaUrl: markerIcon2xModule,
+    shadowUrl: markerShadowModule,
 });
 
 interface MapProps {
-  center?: number[]
+    center?: number[];
 }
 
 const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
-
 const Map: React.FC<MapProps> = ({ center }) => {
     return (
-        <MapContainer 
-          center={center as L.LatLngExpression || [51, -0.09]} 
-          zoom={center ? 4 : 2} 
-          scrollWheelZoom={false} 
-          className="h-[35vh] rounded-lg"
+        <MapContainer
+            center={center as L.LatLngExpression || [51, -0.09]}
+            zoom={center ? 4 : 2}
+            scrollWheelZoom={false}
+            className="h-[35vh] rounded-lg"
         >
-          <TileLayer
-            url={url}
-            attribution={attribution}
-          />
-          {center && (
-            <Marker position={center as L.LatLngExpression} />
-          )}
+            <TileLayer
+                url={url}
+                attribution={attribution}
+            />
+            {center && (
+                <Marker position={center as L.LatLngExpression} />
+            )}
         </MapContainer>
-    )
-  }
-  
-  export default Map
+    );
+}
+
+export default Map;
