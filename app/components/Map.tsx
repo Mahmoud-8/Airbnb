43bb4c2b5e4851f276803c132f03515fccf2d081
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2023-12-27 12:23:52 +0100

    done

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,7 +1,10 @@
 'use client';
 import 'leaflet/dist/leaflet.css';
 
+import markerIcon2x from'leaflet/dist/images/marker-icon-2x.png';
 
+import markerIcon from'leaflet/dist/images/marker-icon.png';
+import markerShadow from'leaflet/dist/images/marker-shadow.png';
 
 import L from 'leaflet';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
@@ -10,7 +13,9 @@ import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 //@ts-ignore
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  
+    iconUrl: markerIcon.src,
+    iconRetinaUrl: markerIcon2x.src,
+    shadowUrl: markerShadow.src,
 });
 
 interface MapProps {
